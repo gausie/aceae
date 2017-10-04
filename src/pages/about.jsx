577334d704174891c9ca791b6a12206b9abd2891
Sam Gaus <sam@gaus.co.uk>
2017-10-05 00:26:17 +0100

    Update about apge

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 export default () => (
   <div>
-    Hi I&apos;m Nat!
+    <p>I&apos;m Nat, an artist based in Edinburgh.</p>
+    <p>
+      I specialise in bold portraits and botanical illustrations in ink,
+      and am currently working on my debut graphic novel.
+    </p>
+    <p>
+      I’m currently available for commissions and collaborations.
+    </p>
+    <p>Get in touch via e-mail: <a href="mailto://hi@notn.at">hi@notn.at</a>.</p>
   </div>
 );
