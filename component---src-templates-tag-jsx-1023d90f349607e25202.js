1fa319342d5415334139904eb5e59dd0c18b96ce
Sam Gaus <sam@gaus.co.uk>
2020-04-13 11:00:22 +0100

    Updates

---
+++ component---src-templates-tag-jsx-1023d90f349607e25202.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{ZUrO:function(e,a,n){"use strict";n.r(a),n.d(a,"default",(function(){return u})),n.d(a,"query",(function(){return i}));var t=n("q1tI"),r=n.n(t),o=n("L6Je"),c=n("ByLX");function u(e){var a=e.data,n=e.location,t=e.pathContext.tag;return r.a.createElement(o.a,{location:n},r.a.createElement(c.a,{pieces:a.allMarkdownRemark.edges,appendTag:t}))}var i="3364896699"}}]);
+//# sourceMappingURL=component---src-templates-tag-jsx-1023d90f349607e25202.js.map
